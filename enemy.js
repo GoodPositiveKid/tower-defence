fc8363dabf9d7855218ac1131a4a2911ce7e91ba
GoodPositiveKid <69410972+GoodPositiveKid@users.noreply.github.com>
2021-03-31 15:14:02 -0700

    Revert "Two paths"

--- enemy.js
+++ enemy.js
@@ -1,54 +1,30 @@
-export let done = null
-export function notDone() {
-    done = null
-}
 export function path1(child,self,drawPath,graphics) {
     graphics = graphics || null;
     drawPath = drawPath || false;
     if (drawPath) {
         graphics.lineStyle(70, 9849600);
         graphics.beginPath();
-        graphics.moveTo(300,20);
-        graphics.lineTo(300,300);
-        graphics.lineTo(599,300);
-        graphics.lineTo(599,589);
+        graphics.moveTo(20,20);
+        graphics.lineTo(20,300);
+        graphics.lineTo(554,300);
+        graphics.lineTo(554,589);
         graphics.strokePath();
     }else {
     if (child.y < 299) {
         child.y+=1
-    } else if ((child.y>298)&&(child.x<599)) {
+    } else if ((child.y>298)&&(child.x<554)) {
         child.x+= 1
-    } else if (child.x > 598) {
+    } else if (child.x > 553) {
         child.y += 1
     };
     if (child.y == 580) {
         self.enemygroup.killAndHide(child);
-        done = "yes"
+        self.numenemys -= 1;
+        self.health -= 4;
     }
-}
-}
-export function path2(child,self,drawPath,graphics) {
-    graphics = graphics || null;
-    drawPath = drawPath || false;
-    if (drawPath) {
-        graphics.lineStyle(70, 9849600);
-        graphics.beginPath();
-        graphics.moveTo(300,20);
-        graphics.lineTo(300,300);
-        graphics.lineTo(30,300);
-        graphics.lineTo(30,589);
-        graphics.strokePath();
-    }else {
-    if (child.y < 299) {
-        child.y+=1
-    } else if ((child.y>298)&&(child.x>29)) {
-        child.x-= 1
-    } else if (child.x < 30) {
-        child.y += 1
-    };
-    if (child.y == 580) {
-        self.enemygroup.killAndHide(child);
-        done = "yes"
+    if (child.y == 400) {
+        self.health += 2;
+        self.numenemys -= 1;
     }
 }
 }
