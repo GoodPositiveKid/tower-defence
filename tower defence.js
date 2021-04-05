4b1f1c3a8e3122ed8a48d87d4248ce9291418794
Minglang Du <mldu@cydu.net>
2021-04-05 10:15:32 -0700

    less money

--- tower defence.js
+++ tower defence.js
@@ -138,6 +138,6 @@ function killEnemy(bullet,enemy){
     this.bulletgroup.killAndHide(bullet);
     enemy.body.enable=false;
     bullet.body.enable=false;
-    inventory.changemoney(2.5);
+    inventory.changemoney(0.5);
     this.numenemys -= 1;
 };
